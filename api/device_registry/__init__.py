d4665ecc10100d5a98a90bffacf665ee5f99bab4
axelzucho <axelzucho1@gmail.com>
2019-04-16 20:01:23 -0500

    Delete Unnecessary info

--- api/device_registry/__init__.py
+++ api/device_registry/__init__.py
@@ -18,18 +18,6 @@ app = Flask(__name__)
 # Create the API
 api = Api(app)
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = shelve.open("devices.db")
-    return db
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
 @app.route("/")
 def index():
     """Present some documentation"""
@@ -83,46 +71,5 @@ class BidirectionalRelation(Resource):
         return {'message': 'Relationship registered', 'data': args}, 201
 
 
-class DeviceList(Resource):
-    def post(self):
-        parser = reqparse.RequestParser()
-
-        parser.add_argument('identifier', required=True)
-        parser.add_argument('name', required=True)
-        parser.add_argument('device_type', required=True)
-        parser.add_argument('controller_gateway', required=True)
-
-        # Parse the arguments into an object
-        args = parser.parse_args()
-
-        shelf = get_db()
-        shelf[args['identifier']] = args
-
-        return {'message': 'Device registered', 'data': args}, 201
-
-
-class Device(Resource):
-    def get(self, identifier):
-        shelf = get_db()
-
-        # If the key does not exist in the data store, return a 404 error.
-        if not (identifier in shelf):
-            return {'message': 'Device not found', 'data': {}}, 404
-
-        return {'message': 'Device found', 'data': shelf[identifier]}, 200
-
-    def delete(self, identifier):
-        shelf = get_db()
-
-        # If the key does not exist in the data store, return a 404 error.
-        if not (identifier in shelf):
-            return {'message': 'Device not found', 'data': {}}, 404
-
-        del shelf[identifier]
-        return '', 204
-
-
-api.add_resource(DeviceList, '/devices')
-api.add_resource(Device, '/device/<string:identifier>')
 api.add_resource(Person, '/person')
 api.add_resource(BidirectionalRelation, '/relation')
