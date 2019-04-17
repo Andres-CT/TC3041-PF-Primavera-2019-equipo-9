6092e04e5de311142731267d1fad7d4946120264
ChrisAAP <christian_aguilar1994@hotmail.com>
2019-04-16 20:29:04 -0500

    Fixed the make persons

--- sensors/person_creator.py
+++ sensors/person_creator.py
@@ -1,35 +1,44 @@
 import random
+import requests
 import os
+import time
 
-#Generate id for the person and increment the counter
-file = open('id_index.txt','r')
-id_person = int(file.readline())
-file.close()
-#if os.path.exists("id_index.txt"):
-  #os.remove("id_index.txt")
-file = open('id_index.txt','w')
-id_person += 1
-file.write(str(id_person))
-file.close()
+id_person = -1
+while(1):
+  #Generate id for the person and increment the counter
 
-#Generate a random number and obtain the corresponding name in the names file
-random_index_name = random.randint(1,100)
-file = open('names.txt','r')
-names = file.readlines()
-name = names[random_index_name - 1].rstrip()
-file.close()
+  #ID method with a text file. IT IS INCOMPLETE
+  # file = open('id_index.txt','r')
+  # id_person = int(file.readline())
+  # file.close()
+  # file = open('id_index.txt','w')
+  # id_person += 1
+  # file.write(str(id_person))
+  # file.close()
 
-#Generate a random age
-random_age = random.randint(0,100)
+  #Easy method for id
+  id_person += 1
 
-#Sent Post request
-#Payload is the json to be sent to the url
-#payload = {}
-#url is the url used for the requests
-#url = ''
-#r = requests.get('',json=payload)
+  #Generate a random number and obtain the corresponding name in the names file
+  random_index_name = random.randint(1,100)
+  file = open('names.txt','r')
+  names = file.readlines()
+  name = names[random_index_name - 1].rstrip()
+  file.close()
 
-#Debugging
-print (id_person)
-print (name)
-print (random_age)
+  #Generate a random age
+  random_age = random.randint(0,100)
+
+  #Sent Post request
+  #Payload is the json to be sent to the url
+  payload = {'id': id_person, 'name':name, 'age':random_age}
+  #url is the url used for the requests
+  url = '172.17.0.1:80'
+  r = requests.post(url,json=payload)
+
+  #Debugging
+  print (id_person)
+  print (name)
+  print (random_age)
+  print(r.text)
+  time.sleep(2)
